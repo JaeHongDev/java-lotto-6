04be2a820307f621d9aec9b231263c6b6e1aec36
JaehongDev <println@kakao.com>
2023-11-03 19:10:43 +0900

    feat: PurchaseLottoComponent 출력 기능 구현

--- src/main/java/lotto/view/component/PurchaseLottoComponent.java
+++ src/main/java/lotto/view/component/PurchaseLottoComponent.java
@@ -1,14 +1,24 @@
 package lotto.view.component;
 
+import java.util.List;
 import lotto.event.EventListener;
+import lotto.state.PurchasedLotto;
+import lotto.state.PurchasedLotto.Lotto;
 import lotto.view.core.Component;
 import lotto.view.core.InputView;
+import lotto.view.core.OutputView;
 
-public record PurchaseLottoComponent(InputView inputView, EventListener eventListener) implements Component {
+public record PurchaseLottoComponent(InputView inputView, OutputView outputView, EventListener eventListener) implements Component {
 
     @Override
     public void render() {
         inputView.readPurchasePrice();
-
+        final var purchasedLotto = new PurchasedLotto(List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6))
+        ));
+        outputView.printPurchasedLotto(purchasedLotto);
     }
 }
--- src/main/java/lotto/view/core/InputView.java
+++ src/main/java/lotto/view/core/InputView.java
@@ -1,11 +1,33 @@
 package lotto.view.core;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public final class InputView extends ConsoleWriter {
 
     public int readPurchasePrice() {
         this.println("구입금액을 입력해 주세요.");
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("에러");
+        }
+    }
+
+    public List<Integer> readAnswerLottoNumber() {
+        this.println("당첨 번호를 입력해 주세요.");
+        try {
+            return Arrays.stream(Console.readLine().split(","))
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("에러");
+        }
+    }
+
+    public int readBonusNumber() {
+        this.println("보너스 번호를 입력해 주세요.");
         try {
             return Integer.parseInt(Console.readLine());
         } catch (Exception exception) {
