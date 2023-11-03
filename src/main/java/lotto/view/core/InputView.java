67a00acb736c82b85e9d6aa0d0747c4f0c32223f
JaehongDev <println@kakao.com>
2023-11-03 13:24:49 +0900

    feat: ConsoleWriter 기능 구현

---
+++ src/main/java/lotto/view/core/InputView.java
@@ -0,0 +1,4 @@
+package lotto.view.core;
+
+public final class InputView extends ConsoleWriter {
+}
---
+++ src/main/java/lotto/view/core/OutputView.java
@@ -0,0 +1,4 @@
+package lotto.view.core;
+
+public final class OutputView extends ConsoleWriter {
+}
