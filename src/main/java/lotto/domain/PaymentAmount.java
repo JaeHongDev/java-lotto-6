d3777d9053557e46d89e4c9313e9a906a70f9588
JaehongDev <println@kakao.com>
2023-11-04 16:06:30 +0900

    feat: 구입금액 엔티티 정의

---
+++ src/main/java/lotto/domain/PaymentAmount.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import lotto.exception.DomainExceptionCode;
+
+public record PaymentAmount(int value) {
+
+    public PaymentAmount {
+        DomainExceptionCode.PAYMENT_IS_GREATER_THAN_MIN_SIZE.dynamicInvokeBy(() -> value < 1000);
+        DomainExceptionCode.PAYMENT_MUST_BE_DIVIDED.dynamicInvokeBy(() -> value % 1000 != 0);
+    }
+
+}
