8c76c1cbd3208cf03b9b4e0a2cb78fb78be4f092
JaehongDev <println@kakao.com>
2023-11-04 17:04:54 +0900

    feat: 정답 로또 입력 기능 구현

--- src/main/java/lotto/component/RegisterAnswerLottoComponent.java
+++ src/main/java/lotto/component/RegisterAnswerLottoComponent.java
@@ -1,13 +1,14 @@
 package lotto.component;
 
 import lotto.event.EventListener;
+import lotto.event.RegisterAnswerLottoEvent;
 import lotto.view.InputView;
 
 public record RegisterAnswerLottoComponent(InputView inputView, EventListener eventListener) implements Component {
 
     @Override
     public void render() {
-
-        final var answerLottoNumber = inputView.readAnswerLottoNumber();
+        eventListener.listenWithParameter(RegisterAnswerLottoEvent::new)
+                .accept(inputView.readAnswerLottoNumber());
     }
 }
