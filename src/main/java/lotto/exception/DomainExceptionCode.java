0dde748c2b7ad9dc1b0611c3de7eee837c58204f
JaehongDev <println@kakao.com>
2023-11-04 17:04:19 +0900

    feat: DomainExceptionCode 메시지 추가

--- src/main/java/lotto/exception/DomainExceptionCode.java
+++ src/main/java/lotto/exception/DomainExceptionCode.java
@@ -4,7 +4,10 @@ import java.util.function.Supplier;
 
 public enum DomainExceptionCode {
     PAYMENT_IS_GREATER_THAN_MIN_SIZE("구입금액은 1000원 부터 구매 가능합니다."),
-    PAYMENT_MUST_BE_DIVIDED("구입금액은 1000원으로 나누어 떨어져야 합니다.");
+    PAYMENT_MUST_BE_DIVIDED("구입금액은 1000원으로 나누어 떨어져야 합니다."),
+
+    LOTTO_NUMBER_IS_GREATER_THAN_MIN_SIZE("로또 번호는 1이상이어야 합니다."),
+    LOTTO_NUMBER_IS_LESS_THAN_MAX_SIZE("로또 번호는 45이하여야 합니다.");
 
 
     private final String message;
