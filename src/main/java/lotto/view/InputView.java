62d221d5fc9fa6f11839912adbe821c634d54158
JaehongDev <println@kakao.com>
2023-11-05 17:34:41 +0900

    feat: RegisterAnswerLottoComponent 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,22 +1,36 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 public final class InputView extends ConsoleWriter {
     public int readPaymentPrice() {
         this.println("구입금액을 입력해 주세요.");
-        return convertStringToInteger().apply("구입금액은 정수여야 합니다.");
+        return convertStringToInteger(Console.readLine()).apply("구입금액은 정수여야 합니다.");
     }
 
-    private Function<String, Integer> convertStringToInteger() {
+    private Function<String, Integer> convertStringToInteger(final String input) {
         return (message) -> {
             try {
-                return Integer.parseInt(Console.readLine());
+                return Integer.parseInt(input);
             } catch (Exception exception) {
                 throw new IllegalArgumentException(message);
             }
         };
     }
 
+    public List<Integer> readAnswerLotto() {
+        this.newLine();
+        this.println("당첨 번호를 입력해 주세요.");
+        return convertStringToListInteger(Console.readLine()).apply("로또 번호는 정수여야 합니다.");
+    }
+
+    private Function<String, List<Integer>> convertStringToListInteger(final String input) {
+        return (message) -> Arrays.stream(input.split(","))
+                .map(this::convertStringToInteger)
+                .map(stringIntegerFunction -> stringIntegerFunction.apply(message))
+                .toList();
+    }
 }
--- src/main/java/lotto/view/MainViewComponents.java
+++ src/main/java/lotto/view/MainViewComponents.java
@@ -3,6 +3,7 @@ package lotto.view;
 import java.util.List;
 import lotto.component.Component;
 import lotto.component.PurchaseLottoComponent;
+import lotto.component.RegisterAnswerLottoComponent;
 import lotto.event.EventListener;
 
 public class MainViewComponents {
@@ -10,7 +11,10 @@ public class MainViewComponents {
     private final List<Component> components;
 
     public MainViewComponents(InputView inputView, OutputView outputView, EventListener eventListener) {
-        this.components = List.of(new PurchaseLottoComponent(inputView, outputView, eventListener));
+        this.components = List.of(
+                new PurchaseLottoComponent(inputView, outputView, eventListener),
+                new RegisterAnswerLottoComponent(inputView, eventListener)
+        );
     }
 
     public void renderAll() {
