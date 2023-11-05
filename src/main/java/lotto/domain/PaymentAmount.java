19e8355144e1380604486af98fe78e7e9a3fbb74
JaehongDev <println@kakao.com>
2023-11-05 22:11:16 +0900

    feat: PaymentAmount 결제 최소 금액 설정 및 결제 가능 금액 설정

---
+++ src/main/java/lotto/domain/PaymentAmount.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import lotto.domain.exception.DomainExceptionCode;
+
+public record PaymentAmount(int value) {
+
+    public PaymentAmount {
+        DomainExceptionCode.PAYMENT_PRICE_IS_GREATER_THAN_MIN_SIZE.dynamicInvokeBy(() -> value < 1000);
+        DomainExceptionCode.PAYMENT_PRICE_IS_GREATER_THAN_MIN_SIZE.dynamicInvokeBy(() -> value % 1000 != 0);
+    }
+}
